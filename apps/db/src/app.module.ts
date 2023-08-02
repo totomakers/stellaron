8435c1f86722c1459703e10c127cb3a299cd56ca
Totomakers <totomakers@gmail.com>
2023-08-02 22:53:52 +0200

    :construction: Setup DB

---
+++ apps/db/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
+
+@Module({
+  imports: [
+    DevtoolsModule.register({
+      http: process.env.NODE_ENV !== 'production',
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
