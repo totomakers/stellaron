fb67637cfd4558a500b30e19cbb747377ab68af2
Totomakers <totomakers@gmail.com>
2023-08-06 11:55:26 +0200

    bug(stellaron): Fix db

--- apps/stellaron-api/src/mikro-orm.config.ts
+++ apps/stellaron-api/src/mikro-orm.config.ts
@@ -6,6 +6,9 @@ const config = defineConfig({
   clientUrl: process.env.MIKRO_ORM_CLIENT_URL,
   entities: ['./dist/db/entities'],
   entitiesTs: ['./src/db/entities'],
+  driverOptions: {
+    ssl: 'require',
+  },
 });
 
 export default config;
